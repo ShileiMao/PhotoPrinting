de5e147c4d131b645db9f03f714e791cc9f99230
Shilei Mao <shileimao@Shileis-MacBook-Air.local>
2022-05-28 17:34:12 +0100

    ---

--- backend/photo/src/main/java/com/pdd/photoprint/photo/controllers/IndexController.java
+++ backend/photo/src/main/java/com/pdd/photoprint/photo/controllers/IndexController.java
@@ -1,13 +1,15 @@
 package com.pdd.photoprint.photo.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import javax.servlet.http.HttpServletRequest;
 
+@Controller
 public class IndexController {
-//    @RequestMapping("/")
-//    public void index() {
-//        r
-//    }
+    @RequestMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}" })
+    public String getIndex(HttpServletRequest request) {
+        return "/index.html";
+    }
 }
