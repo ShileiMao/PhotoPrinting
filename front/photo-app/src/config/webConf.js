d909a7e0e3d4524098e29bca52b78e98b1a9c384
Shilei Mao <shileimao@SHILEIS-AIR.station>
2022-06-08 10:30:31 +0100

    add build

--- front/photo-app/src/config/webConf.js
+++ front/photo-app/src/config/webConf.js
@@ -1,8 +1,8 @@
 const Config = {
-    BASE_URL: "http://127.0.0.1:8000",
+    // BASE_URL: "http://127.0.0.1:8000",
     // BASE_URL: "http://10.54.145.219:8000",
 
-    // BASE_URL: "http://39.105.223.206:8080/photo",
+    BASE_URL: "http://39.105.223.206:8080/photo",
 
     // BASE_URL: "http://192.168.1.16:8000",
 
